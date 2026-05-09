70bc476d762523125bb75a11101906568a154ee0
Agustín Fernández <fernandez.agus2000@gmail.com>
2026-05-08 21:26:31 -0300

    prueba drawer

--- servidor/assets/js/drawer.js
+++ servidor/assets/js/drawer.js
@@ -3,22 +3,28 @@ const drawer = document.querySelector(".drawer");
 const overlay = document.querySelector(".drawer-overlay");
 const closeBtn = document.querySelector(".drawer-close");
 
-function openDrawer() {
-  drawer.classList.add("open");
-  overlay.classList.add("open");
+if (fab && drawer && overlay && closeBtn) {
 
-  document.body.style.overflow = "hidden";
+  function openDrawer() {
+
+    drawer.classList.add("open");
+    overlay.classList.add("open");
+
+    document.body.style.overflow = "hidden";
+  }
+
+  function closeDrawer() {
+
+    drawer.classList.remove("open");
+    overlay.classList.remove("open");
+
+    document.body.style.overflow = "";
+  }
+
+  fab.addEventListener("click", openDrawer);
+
+  closeBtn.addEventListener("click", closeDrawer);
+
+  overlay.addEventListener("click", closeDrawer);
+
 }
-
-function closeDrawer() {
-  drawer.classList.remove("open");
-  overlay.classList.remove("open");
-
-  document.body.style.overflow = "";
-}
-
-fab.addEventListener("click", openDrawer);
-
-closeBtn.addEventListener("click", closeDrawer);
-
-overlay.addEventListener("click", closeDrawer);
