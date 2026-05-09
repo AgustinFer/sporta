ae06447dc61a2e3b542259986f437629b4a3103e
Agustín Fernández <fernandez.agus2000@gmail.com>
2026-05-08 21:21:06 -0300

    agrego primera versión del drawer

---
+++ servidor/assets/js/drawer.js
@@ -0,0 +1,24 @@
+const fab = document.querySelector(".fab");
+const drawer = document.querySelector(".drawer");
+const overlay = document.querySelector(".drawer-overlay");
+const closeBtn = document.querySelector(".drawer-close");
+
+function openDrawer() {
+  drawer.classList.add("open");
+  overlay.classList.add("open");
+
+  document.body.style.overflow = "hidden";
+}
+
+function closeDrawer() {
+  drawer.classList.remove("open");
+  overlay.classList.remove("open");
+
+  document.body.style.overflow = "";
+}
+
+fab.addEventListener("click", openDrawer);
+
+closeBtn.addEventListener("click", closeDrawer);
+
+overlay.addEventListener("click", closeDrawer);
