7f5e5d04271123ea022d7576813b2ece9a114384
Agustín Fernández <fernandez.agus2000@gmail.com>
2026-05-21 16:30:29 -0300

    cambio rutas relativas a las páginas

--- servidor/canchas/index.php
+++ servidor/canchas/index.php
@@ -6,13 +6,13 @@
   <title>Sporta - Canchas</title>
 
   <!-- CSS GLOBAL -->
-  <link rel="stylesheet" href="/assets/css/global.css">
-  <link rel="stylesheet" href="/assets/css/layout.css">
-  <link rel="stylesheet" href="/assets/css/components.css">
-  <link rel="stylesheet" href="/assets/css/drawer.css">
+  <link rel="stylesheet" href="../assets/css/global.css">
+  <link rel="stylesheet" href="../assets/css/layout.css">
+  <link rel="stylesheet" href="../assets/css/components.css">
+  <link rel="stylesheet" href="../assets/css/drawer.css">
 
   <!-- CSS MÓDULO -->
-  <link id="page-style" rel="stylesheet" href="/canchas/canchas.css">
+  <link id="page-style" rel="stylesheet" href="canchas.css">
 </head>
 
 <body class="screen" data-page="Canchas">
@@ -95,10 +95,10 @@
   <div id="drawer-container"></div>
 
   <!-- JS LAYOUT (carga sidebar + header) -->
-  <script src="/assets/js/layout.js"></script>
+  <script src="../assets/js/layout.js"></script>
 
   <!-- JS DRAWER -->
-  <script src="/assets/js/drawer.js"></script>
+  <script src="../assets/js/drawer.js"></script>
   <!-- FIN CONFIG GLOBAL -->
 
 </body>
