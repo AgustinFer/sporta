009354cf731a49c69439534ef21e3bc21c0fbc79
Agustín Fernández <fernandez.agus2000@gmail.com>
2026-05-21 17:00:21 -0300

    agrego rutas relativas a inicio

--- servidor/inicio/index.php
+++ servidor/inicio/index.php
@@ -6,13 +6,13 @@
   <title>Sporta - Inicio</title>
 
   <!-- CSS GLOBAL -->
-  <link rel="stylesheet" href="/assets/css/global.css">
-  <link rel="stylesheet" href="/assets/css/layout.css">
-  <link rel="stylesheet" href="/assets/css/components.css">
-  <link rel="stylesheet" href="/assets/css/drawer.css">
+  <link rel="stylesheet" href="../assets/css/global.css">
+  <link rel="stylesheet" href="../assets/css/layout.css">
+  <link rel="stylesheet" href="../assets/css/components.css">
+  <link rel="stylesheet" href="../assets/css/drawer.css">
 
   <!-- CSS MÓDULO -->
-  <link id="page-style" rel="stylesheet" href="/inicio/inicio.css">
+  <link id="page-style" rel="stylesheet" href="inicio.css">
 </head>
 
 <body class="screen" data-page="Inicio">
@@ -111,10 +111,10 @@
   <div id="drawer-container"></div>
 
   <!-- JS LAYOUT (carga sidebar + header) -->
-  <script src="/assets/js/layout.js"></script>
+  <script src="../assets/js/layout.js"></script>
 
   <!-- JS DRAWER -->
-  <script src="/assets/js/drawer.js"></script>
+  <script src="../assets/js/drawer.js"></script>
   <!-- FIN CONFIG GLOBAL -->
 
 </body>
