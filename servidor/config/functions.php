226f17e235049066443cf35e3b1789fd191723a4
Agustín Fernández <fernandez.agus2000@gmail.com>
2026-05-22 00:49:32 -0300

    agrego DIR a los require

--- servidor/config/functions.php
+++ servidor/config/functions.php
@@ -1,5 +1,5 @@
 <?php
-require "conexion.php";
+require_once __DIR__ . '/conexion.php';
 
 function login($email, $pass){
 
--- servidor/turnos/index.php
+++ servidor/turnos/index.php
@@ -1,4 +1,4 @@
-<?php require_once '../config/init.php'; ?>
+<?php require_once __DIR__ . '/../config/init.php'; ?>
 
 <!DOCTYPE html>
 <html lang="es">
