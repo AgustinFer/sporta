cccf7af725053383d521d4ce427d3a2c99741d61
Agustín Fernández <fernandez.agus2000@gmail.com>
2026-05-22 16:51:59 -0300

    agrega validación de usuario a las páginas

--- servidor/clientes/index.php
+++ servidor/clientes/index.php
@@ -1,5 +1,10 @@
 <?php
 
+if(!isset($_SESSION['usuario'])){
+  header("Location: ../index.php");
+  exit;
+}
+
 require_once __DIR__ . '/../config/init.php';
 require_once __DIR__ . '/../config/conexion.php';
 
