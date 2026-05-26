db81977adc604bce8707c35130dda71abff026d1
Agustín Fernández <fernandez.agus2000@gmail.com>
2026-05-26 00:36:26 -0300

    agrego debug index

--- servidor/index.php
+++ servidor/index.php
@@ -1,8 +1,10 @@
-<?php require_once __DIR__ . '/config/init.php';
+<?php 
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+require_once __DIR__ . '/config/init.php';
 
 if(isset($_POST['iniciar'])){
     Usuario::iniciarSesion($_POST['email'],$_POST['password']);
