e439aef84118eccaefef3b3687faab9e2a22dc08
Agustín Fernández <fernandez.agus2000@gmail.com>
2026-06-15 14:48:20 -0300

    agregar script faltante estado_cancha Inhabilitado

---
+++ estado_adicional.sql
@@ -0,0 +1,3 @@
+-- Estado de cancha faltante (agregado después del PR de Canchas y Reservas)
+INSERT INTO estado_cancha (estado_cancha_id, descripcion, observaciones)
+VALUES (3, 'Inhabilitado', NULL);
